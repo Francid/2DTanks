ed2495dce3d708d6df14b7afef6cb9be8fa7ce7b
francid <francid81@gmail.com>
2016-03-02 20:09:34 -0500

    Initial commit

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+	// PUBLIC INSTANCE VARIABLES
+	public int tankCount;
+	public GameObject tank;
+	public Text livesLabel;
+	public Text scoreLabel;
+	public Text highScoreLabel;
+	public Text gameOverLabel;
+	public Button resetButton;
+
+	// PRIVATE INSTANCE VARIABLES
+	private int _scoreValue;
+	private int _livesValue;
+
+	//Access methods
+	public int ScoreValue{
+		get{ 
+			return this._scoreValue;
+		}
+		set{ 
+			this._scoreValue = value;
+			this.scoreLabel.text = "Score: " + this._scoreValue;
+		}
+	}
+
+	public int LivesValue{
+		get{ 
+			return this._livesValue;
+		}
+		set{ 
+			this._livesValue = value;
+			if (this._livesValue < 0) {
+				this.highScoreLabel.text = "High Score: " + this._scoreValue;
+				this.gameOverScene ();
+			}
+			this.livesLabel.text = "Lives: " + this._livesValue;
+		}
+	}
+	
+	// Use this for initialization
+	void Start () {
+		this.ScoreValue = 0;
+		this.LivesValue = 3;
+		this.gameOverLabel.gameObject.SetActive (false);
+		this.resetButton.gameObject.SetActive (false);
+		this.highScoreLabel.gameObject.SetActive (false);
+		this._GenerateTanks ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// generate Tanks
+	private void _GenerateTanks() {
+		
+		for (int count=0; count < this.tankCount; count++) {
+			Instantiate(tank);
+		}
+	}
+
+	private void gameOverScene(){
+
+		this.scoreLabel.gameObject.SetActive (false);
+		this.livesLabel.gameObject.SetActive (false);
+		this.gameOverLabel.gameObject.SetActive (true);
+		this.resetButton.gameObject.SetActive (true);
+		this.highScoreLabel.gameObject.SetActive (true);
+
+	}
+
+
+	public void ResetButton(){
+		Application.LoadLevel ("Main");
+	}
+}
---
+++ Assets/_Scripts/PlayerCollider.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollider : MonoBehaviour {
+
+	//Public Variable
+	public EnemyController enemyController;
+	public GameController gameController;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void OnTriggerEnter2D(Collider2D other){
+
+		if(other.gameObject.CompareTag("Enemy")){
+			//Reset the position to default
+			enemyController._Reset();
+			gameController.LivesValue -= 1;
+		}
+	}
+}
